d2da8c6cb555800520dcbb42c9ae887692fc61ad
abakunov <abakunovkirill@outlook.com>
2022-09-17 14:31:31 +0300

    CORS problem solved

--- hackzurich/settings.py
+++ hackzurich/settings.py
@@ -37,15 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 
     'core',
     'api',
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -71,6 +73,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'hackzurich.wsgi.application'
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 # Database
